0ed612eb31f86689992bc610c90d936306aed771
Alex Forsythe <alex.forsythe@uber.com>
2017-08-04 14:26:36 -0700

    Updated directory structure to separate client from server code

--- post_build.sh
+++ post_build.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-temp_bundle="bundle.min.js.temp"
-bundle="bundle.min.js"
+temp_bundle="client/bundle.min.js.temp"
+bundle="client/bundle.min.js"
 
 echo "<script>" > ${temp_bundle}
-cat "bundle.min.js" >> ${temp_bundle}
+cat ${bundle} >> ${temp_bundle}
 echo "</script>" >> ${temp_bundle}
 mv ${temp_bundle} ${bundle}
