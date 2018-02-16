7111c6013c7d52fe3eb3fc1502dcd6ff6e18c563
Alex Forsythe <alex.forsythe@uber.com>
2018-02-15 21:54:56 -0800

    Removed commented reference to ScriptApp to fix permissions: fixes #58

--- server/main.js
+++ server/main.js
@@ -13,8 +13,8 @@
  * Creates a menu entry in the Google Docs UI when the document is opened.
  *
  * @param {Object} e The event parameter for a simple onOpen trigger. To
- *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
- *     running in, inspect e.authMode.
+ *     determine which authorization mode the trigger is running in, inspect
+ *     e.authMode.
  */
 function onOpen(e) {
     DocumentApp.getUi().createAddonMenu()
